e59a7971605997fe665a4af08fb6cd2f920427c8
anaclara-dias <acod2105@gmail.com>
2025-05-08 10:28:35 -0300

    07/05

---
+++ exercicioscomponentes/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App()
+{
+  return(
+    <div>
+      <h1> Exercicios de Componentes</h1>
+       
+    </div>
+  )
+}
---
+++ exercicioscomponentes/src/Componentes/Exemplo1.jsx
@@ -0,0 +1,4 @@
+export default function Exemplo1({numero1, numero2})
+{
+    
+}
