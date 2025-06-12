156fb51ab17dd172497dd4cc6e900ef0a75a4fda
anaclara-dias <acod2105@gmail.com>
2025-06-12 10:26:09 -0300

    12/06

--- estudocss/src/Paginas/Exercicio1.jsx
+++ estudocss/src/Paginas/Exercicio1.jsx
@@ -2,9 +2,9 @@ export default function Exercicio1()
 {
     return(
     <div>
-        <h2 style ={{border: "10px", color: "red"}}> Exercicio 1</h2>
+        <h2 style ={{textAlign: "center", border: "10px", color: "red"}}> Exercicio 1</h2>
         <p>
-            Este paragrafo esta na cor + "cor"
+            Este paragrafo esta na cor 
         </p>
 
     </div>
