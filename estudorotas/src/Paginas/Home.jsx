80429c923da1b67558797904cbe5bbcdbe4bb2ae
anaclara-dias <acod2105@gmail.com>
2025-08-14 08:59:49 -0300

    1408

--- estudorotas/src/Paginas/Home.jsx
+++ estudorotas/src/Paginas/Home.jsx
@@ -9,23 +9,31 @@ export default function Home() {
                 <li><Link to = "/AC" >Acre</Link></li>
                 <li><Link to = "/AL" >Alagoas</Link></li>
                 <li><Link to = "/AP" >Amapá</Link></li>
-                <li><Link to = "/SP" >Amazonas</Link></li>
-                <li><Link to = "/SP" >Bahia</Link></li>
-                <li><Link to = "/SP" >Ceara</Link></li>
-                <li><Link to = "/SP" >Distrito Federal</Link></li>
+                <li><Link to = "/AM" >Amazonas</Link></li>
+                <li><Link to = "/BA" >Bahia</Link></li>
+                <li><Link to = "/CE" >Ceara</Link></li>
+                <li><Link to = "/DF" >Distrito Federal</Link></li>
                 <li><Link to = "/ES" >Espírito Santo</Link></li>
-                <li><Link to = "/SP" >Goiás</Link></li>
-                <li><Link to = "/SP" >Maranhão</Link></li>
-                <li><Link to = "/SP" >Mato Grosso</Link></li>
-                <li><Link to = "/SP" >Mato Grosso Do Sul</Link></li>
+                <li><Link to = "/GO" >Goiás</Link></li>
+                <li><Link to = "/MA" >Maranhão</Link></li>
+                <li><Link to = "/MT" >Mato Grosso</Link></li>
+                <li><Link to = "/MS" >Mato Grosso Do Sul</Link></li>
                 <li><Link to = "/MG" >Minas Gerais</Link></li>
-                <li><Link to = "/SP" >São Paulo</Link></li>
+                <li><Link to = "/PA" >Pará</Link></li>
+                <li><Link to = "/PB" >Paraíba</Link></li>
+                <li><Link to = "/PE" >Pernambuco</Link></li>
+                <li><Link to = "/PI" >Piauí</Link></li>
                 <li><Link to = "/RJ" >Rio de Janeiro</Link></li>
-                <li><Link to = "/SC" >Santa Catarina</Link></li>
-                <li><Link to = "/MG" >Minas Gerais</Link></li>
-                <li><Link to = "/PR" >Paraná</Link></li>
-                
+                <li><Link to = "/RN" >Rio Grande do Norte</Link></li>
                 <li><Link to = "/RS" >Rio Grande do Sul</Link></li>
+                <li><Link to = "/RO" >Rondônia</Link></li>
+                <li><Link to = "/RR" >Roraima</Link></li>
+                <li><Link to = "/SC" >Santa Catarina</Link></li>
+                <li><Link to = "/SP" >São Paulo</Link></li>
+                <li><Link to = "/SE" >Sergipe</Link></li>
+                <li><Link to = "/TO" >Tocantins</Link></li>
+                
+               
             </ul>
         </div>
     )
